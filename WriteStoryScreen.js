8efdf25f507f8533a150a05b734ceec99db6a8c5
ptr25 <62867142+ptr25@users.noreply.github.com>
2020-11-30 16:17:50 +0530

    Add files via upload

---
+++ WriteStoryScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+
+
+
+export default class WriteStoryScreen extends React.Component{
+    render(){
+        return(
+<Text>Write Story</Text>
+        );
+    }
+
+}
